cbe3003fe04f8ccb14ca2c60bc16c002648dfc2c
Jason <sanchezjason@me.com>
2025-06-11 15:48:42 -0600

    AllocationLedgerModel, added entity's

--- AllocationLedger/AllocationLedger/AllocationLedgerApp.swift
+++ AllocationLedger/AllocationLedger/AllocationLedgerApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct AllocationLedgerApp: App {
+    let provider: CoreDataProvider
+    
+    init() {
+        provider = CoreDataProvider()
+    }
+    
     var body: some Scene {
         WindowGroup {
             AllocationView()
+                .environment(\.managedObjectContext, provider.context)
         }
     }
 }
